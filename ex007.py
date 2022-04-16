1dbaf6d1850e0192b1968bcc4c58a2dcfad369f2
theVitor00 <thevitor00@gmail.com>
2022-04-16 18:18:17 -0300

    Create ex007.py

---
+++ ex007.py
@@ -0,0 +1,10 @@
+# DESENVOLVA UM PROGRAMA QUE LEIA AS DUAS NOTAS DE UM ALUNO. CALCULE E MOSTRE A SUA MÉDIA.
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+
+med = (n1 + n2) / 2
+
+print('A média do aluno nessa matéria é igual a {:.1f}'.format(med))
+
+
