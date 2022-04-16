1a1839ac877b28efec32968258d913a07397e304
theVitor00 <thevitor00@gmail.com>
2022-04-16 18:06:00 -0300

    Update ex005.py

---
+++ ex005.py
@@ -0,0 +1,4 @@
+# FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO E MOSTRE NA TELA O SEU SUCESSOR E O SEU ANTECESSOR
+
+num = int(input('Digite um número: '))
+print('O número digitado foi {}. Seu antecessor é {}, e o seu sucessor é {}.'.format(num, num - 1, num + 1))
