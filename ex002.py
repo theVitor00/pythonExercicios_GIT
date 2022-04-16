57339116c0997cdcc0de9cd44877304dcad6b721
theVitor00 <thevitor00@gmail.com>
2022-04-16 17:31:06 -0300

    Create ex002.py

---
+++ ex002.py
@@ -0,0 +1,4 @@
+# FAÇA UM PROGRAMA QUE LEIA O NOME DE UMA PESSOA E MOSTRE UMA MENSAGEM DE BOAS VINDAS
+
+nome = str(input('Digite o seu nome: '))
+print('Seja bem vindo, {}, é um prazer te conhecer'.format(nome))
