88e086525b99e68465e9bfca9b0e4245d13a2fc7
theVitor00 <thevitor00@gmail.com>
2022-04-16 17:34:55 -0300

    Create ex003.py

---
+++ ex003.py
@@ -0,0 +1,6 @@
+# CRIE UM PROBRAMA QUE LEIA DOIS NÚMEROS E MOSTRE A SOMA ENTRE ELES
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+print('A soma entre {} e {} é igual a {}.'.format(n1, n2, n1 + n2))
