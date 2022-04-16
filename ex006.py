df643b3e17f5c45cb193d6395ce75f483841892c
theVitor00 <thevitor00@gmail.com>
2022-04-16 18:12:20 -0300

    Create ex006.py

---
+++ ex006.py
@@ -0,0 +1,6 @@
+# CRIE UM ALGORITMO QUE LEIA UM NÚMERO E MOSTRE O SEU DOBRO, TRIPLO E RAIZ QUADRADA
+
+num = int(input('Digite um número: '))
+print('O dobro de {} é igual a {}.'.format(num, num * 2))
+print('Seu triplo é igual a {}'.format(num * 3))
+print('A sua raiz quadrada é igual a {}'.format(num**0.5))
