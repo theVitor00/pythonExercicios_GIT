ed944200cc9945be9141b27bbf3a0c91efc24fc5
theVitor00 <thevitor00@gmail.com>
2022-04-17 00:13:16 -0300

    Incremento do exercício
    Proposta de ampliação do exercício por parte do professor

--- ex008.py
+++ ex008.py
@@ -1,4 +1,14 @@
 # ESCREVA UM PROGRAMA QUE LEIA UM VALOR EM METROS E O EXIBA EM CENTÍMETROS E EM MILÍMETROS
 
-num = int(input('Digite um valor em metros: '))
-print('{} em Metros equivale a {}cm e a {}mm.'.format(num, num * 100, num * 1000))
+num = float(input('Digite um valor em metros: '))
+dm = num * 10
+cm = num * 100
+mm = num * 1000
+dam = num / 10
+hm = num / 100
+km = num / 1000
+
+
+
+print('{} em Metros equivale a {}Dm, {}Cm e a {}Mm, '.format(num, dm, cm, mm))
+print('{}Dam, {}Hm e {}Km'.format(dam, hm, km))
