13ab8d75463b85c13fbe1e50c499a15da21736bb
theVitor00 <thevitor00@gmail.com>
2022-04-17 21:55:44 -0300

    Update ex014.py

--- ex014.py
+++ ex014.py
@@ -4,4 +4,3 @@ celsius = float(input('Digite a temperatura em Celsius: '))
 fahrenheit = (celsius * (9 / 5) + 32)
 
 print('{:.1f} graus Celsius equivale a {:.1f} graus Fahrenheit'.format(celsius, fahrenheit))
-
