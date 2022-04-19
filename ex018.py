ac8559ebf09eddb441dc8dcb76ab3ea24137d4f7
theVitor00 <thevitor00@gmail.com>
2022-04-18 23:33:15 -0300

    Update ex018.py
    Adicionado conversão da variavel angulo para Radiano

--- ex018.py
+++ ex018.py
@@ -1,11 +1,11 @@
 # FAÇA UM PROGRAMA QUE LEIA UM ÂNGULO QUALQUER E MOSTRE NA TELA O VALOR DO SEU SENO, COSSENO E TANGENTE
 
-from math import sin, cos, tan
+from math import sin, cos, tan, radians
 
 angulo = int(input('Digite o valor do ângulo a ser analisado: '))
 print('-' * 41)
 print('==== Analisando o ângulo de {} graus ===='.format(angulo))
-print('SENO: {:.2f}'.format(sin(angulo)))
-print('COSSENO: {:.2f}'.format(cos(angulo)))
-print('TANGENTE: {:.2f}'.format(tan(angulo)))
+print('SENO: {:.2f}'.format(sin(radians(angulo))))
+print('COSSENO: {:.2f}'.format(cos(radians(angulo))))
+print('TANGENTE: {:.2f}'.format(tan(radians(angulo))))
 print('-' * 41)
