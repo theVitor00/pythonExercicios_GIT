6ad31bc0ea70a779b6fd411a4da1a743b0c0c705
theVitor00 <thevitor00@gmail.com>
2022-04-21 11:35:54 -0300

    Create ex020.py

---
+++ ex020.py
@@ -0,0 +1,14 @@
+# O MESMO PROFESSOR DO DESAFIO ANTERIOR QUER SORTEAR A ORDEM DA APRESENTAÇÃO DE TRABALHO DOS ALUNOS. FAÇA UM PROGRAMA
+# QUE LEIA O NOME DOS QUATRO ALUNOS E MOSTRE A ORDEM SORTEADA.
+
+import random
+
+a1 = str(input('Digite o nome do primeiro aluno: '))
+a2 = str(input('Digite o nome do segundo aluno: '))
+a3 = str(input('Digite o nome do terceiro aluno: '))
+a4 = str(input('Digite o nome do quarto aluno: '))
+
+alunos = [a1, a2, a3, a4]
+random.shuffle(alunos)
+
+print('A ordem de apresentação dos alunos será: {}'.format(alunos))
