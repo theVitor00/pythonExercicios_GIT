a1d5ed7b40acf81904c7efbe9e003e2ec670fea0
theVitor00 <thevitor00@gmail.com>
2022-04-21 12:14:47 -0300

    Update ex021.py
    De acordo com os comentários no video do exercício, de pois de algumas atualizações no pygame, devemos primeiro iniciar o mixer:
     > pygame.mixer.init()
    e só então iniciamos o pygame em si
    > pygame.init()
    
    depois dessa modificação, o código funcionou perfeitamente

--- ex021.py
+++ ex021.py
@@ -1,6 +1,7 @@
 # FAÇA UM PROGRAMA EM PYTHON QUE ABRA E REPRODUZA UM PROGRAMA EM MP3
 
 import pygame
+pygame.mixer.init()
 pygame.init()
 pygame.mixer.music.load('song.mp3')
 pygame.mixer.music.play()
