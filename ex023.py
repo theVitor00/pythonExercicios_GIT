3495638b569dee079c20c909e8aa82c8023cb1bb
theVitor00 <thevitor00@gmail.com>
2022-04-21 22:03:47 -0300

    Update ex023.py
    Não consegui responder, então segui a resposta do proferror.

--- ex023.py
+++ ex023.py
@@ -4,3 +4,20 @@
 # DEZENA: 3
 # CENTENA: 8
 # MILHAR: 1
+
+num = int(input('Digite um número entre 0 e 9999: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 1000
+
+print('Analisando o número {} ...'.format(num))
+print('UNIDADE: {}'.format(u))
+print('DEZENA: {}'.format(d))
+print('CENTENA: {}'.format(c))
+print('MILHAR: {}'.format(m))
+
+
+
+
+
