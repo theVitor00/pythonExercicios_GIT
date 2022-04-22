c33d20b1b35454e84e8bfaac5767f4c92d130b05
theVitor00 <thevitor00@gmail.com>
2022-04-21 22:12:58 -0300

    Update ex024.py

--- ex024.py
+++ ex024.py
@@ -1,1 +1,5 @@
 # CRIE UM PROGRAMA QUE LEIA O NOME DE UMA CIDADE E DIGA SE ELA COMEÇA OU NÃO COM A PALAVRA 'SANTO'
+
+cidade = str(input('Digite o nome de uma cidade: '))
+cidade = cidade.split()
+print('A cidade começa com o nome \'Santo\'? - {}'.format('Santo' in cidade[0]))
