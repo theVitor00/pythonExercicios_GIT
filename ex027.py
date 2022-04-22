425b48c887238519260f935796fac74067d36efa
theVitor00 <thevitor00@gmail.com>
2022-04-21 22:53:47 -0300

    Update ex027.py
    Solução que escrevi para o exercício 27. Parece estar funcionando corretamente após alguns testes

--- ex027.py
+++ ex027.py
@@ -1,1 +1,6 @@
 # FAÇA UM PROGRAMA QUE LEIA O NOME COMPLETO DE UMA PESSOA E MOSTRE EM SEGUIDA O PRIMEIRO E O ULTIMO NOME SEPARADAMENTE
+
+nome = str(input('Digite seu nome completo: '))
+print('Analisando...')
+print('O seu primeiro nome é: {}'.format(nome.split()[0]))
+print('O seu último nome é: {}'.format(nome.split()[-1]))
