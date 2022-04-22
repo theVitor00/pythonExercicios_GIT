7acf788b0998975cd7d93608aefba4ecbedad62d
theVitor00 <thevitor00@gmail.com>
2022-04-21 22:48:57 -0300

    Update ex026.py
    Solução do exercício 26. Aparentemente está funcionando tudo perfeitamente

--- ex026.py
+++ ex026.py
@@ -2,3 +2,12 @@
 # >> QUANTAS VEZES APARECE A LETRA 'A'
 # >> EM QUE POSIÇÃO ELA APARECE A PRIMEIRA VEZ (FIND)
 # >> EM QUE POSIÇÃO ELA APARECE A ÚLTIMA VEZ
+
+frase = str(input('Digite uma frase: '))
+print('Analisando...')
+print('A letra \'A\' aparece {} vezes nessa frase.'.format(frase.count('A')))
+print('Ela aparece pela primeira vez na posição {}.'.format(frase.find('A')))
+print('E aparece pela última vez na posição {}.'.format(frase.find('A', frase.count('A') + frase.find('A'))))
+
+
+
