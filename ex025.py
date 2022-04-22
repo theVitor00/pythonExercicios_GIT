96a38e420a116c38e5f3c1c1d989670534e8cafd
theVitor00 <thevitor00@gmail.com>
2022-04-21 22:17:16 -0300

    Update ex025.py

--- ex025.py
+++ ex025.py
@@ -1,1 +1,5 @@
 # CRIE UM PROGRAMA QUE LEIA O NOME DE UMA PESSOA E DIGA SE ELA TEM 'SILVA' NO NOME
+
+nome = str(input('Digite seu nome completo: '))
+print('Analisando...')
+print('Contém \'Silva\' no nome: {}'.format('Silva' in nome))
