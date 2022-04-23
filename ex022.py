b0f2834324cc39e386dbd85f6d7d0b86ebabcf38
theVitor00 <thevitor00@gmail.com>
2022-04-22 23:14:11 -0300

    Update ex022.py
    adicionado um médtodo .strip() na definição das variáveis para eliminar os expaços dos extremos

--- ex022.py
+++ ex022.py
@@ -3,13 +3,10 @@
 # >> QUANTAS LETRAS TEM NO TOTAL, SEM CONSIDERAR OS ESPAÇOS
 # >> QUANTAS LETRAS TEM O PRIMEIRO NOME
 
-nome = str(input('Qual é o seu nome completo: '))
+nome = str(input('Qual é o seu nome completo: ')).strip()
 print('\nAnalisando seu nome...')
 print('{}'.format(nome.upper()))
 print('{}'.format(nome.lower()))
-#espaco = nome.count(' ')
 print('Seu nome tem {} letras.'.format(len(nome) - nome.count(' ')))
 nome = nome.split()
 print('Seu primeiro nome possui {} letras.'.format(len(nome[0])))
-
-
