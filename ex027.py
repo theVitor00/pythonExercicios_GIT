a8bdd0d60e36c89a58ba673d49c77da3739ad38a
theVitor00 <thevitor00@gmail.com>
2022-04-22 23:43:57 -0300

    update
    Decidi não alterar meu código porque acredito q seja mais eficiente, mas fiz um comentário indicando outra resposta possível

--- ex027.py
+++ ex027.py
@@ -1,6 +1,8 @@
 # FAÇA UM PROGRAMA QUE LEIA O NOME COMPLETO DE UMA PESSOA E MOSTRE EM SEGUIDA O PRIMEIRO E O ULTIMO NOME SEPARADAMENTE
 
-nome = str(input('Digite seu nome completo: '))
+nome = str(input('Digite seu nome completo: ')).strip()
 print('Analisando...')
 print('O seu primeiro nome é: {}'.format(nome.split()[0]))
 print('O seu último nome é: {}'.format(nome.split()[-1]))
+
+# outra solução para mostrar o último nome de uma string é usar o .format(variavel[len(variavel) - 1])
