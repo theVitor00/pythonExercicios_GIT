5866e27c19efc6c200e77b449818a7cb7d4b99cf
theVitor00 <thevitor00@gmail.com>
2022-04-22 23:28:04 -0300

    Update ex025.py
    O meu não estava errado, mas fiz as otimizações do professor

--- ex025.py
+++ ex025.py
@@ -1,5 +1,6 @@
 # CRIE UM PROGRAMA QUE LEIA O NOME DE UMA PESSOA E DIGA SE ELA TEM 'SILVA' NO NOME
 
-nome = str(input('Digite seu nome completo: '))
+nome = str(input('Digite seu nome completo: ')).strip()
 print('Analisando...')
-print('Contém \'Silva\' no nome: {}'.format('Silva' in nome))
+print('Contém \'Silva\' no nome: {}'.format('SILVA' in nome.upper()))
+
