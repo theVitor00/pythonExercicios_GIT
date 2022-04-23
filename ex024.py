9b50a06cb39d0059f05d33c0506d794f43dc0046
theVitor00 <thevitor00@gmail.com>
2022-04-22 23:22:39 -0300

    update
    Adicionado solução do professor

--- ex024.py
+++ ex024.py
@@ -1,5 +1,12 @@
 # CRIE UM PROGRAMA QUE LEIA O NOME DE UMA CIDADE E DIGA SE ELA COMEÇA OU NÃO COM A PALAVRA 'SANTO'
 
-cidade = str(input('Digite o nome de uma cidade: '))
-cidade = cidade.split()
-print('A cidade começa com o nome \'Santo\'? - {}'.format('Santo' in cidade[0]))
+#cidade = str(input('Digite o nome de uma cidade: '))
+#cidade = cidade.split()
+#print('A cidade começa com o nome \'Santo\'? - {}'.format('Santo' in cidade[0]))
+
+########################################################################################################################
+#                                                   RESOLUÇÃO DO PROFESSOR                                             #                      #
+########################################################################################################################
+
+cid = str(input('Em que cidade voce nasceu: ')).strip()
+print(cid[:5].upper() == 'SANTO')
