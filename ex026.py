ad1c99c784cfda9ead09bb1f6ba0af775d948687
theVitor00 <thevitor00@gmail.com>
2022-04-22 23:35:52 -0300

    Update ex026.py
    Adicionado resulução do professor. O meu estava funcionando corretamente, mas a versão dele é mais eficiente

--- ex026.py
+++ ex026.py
@@ -3,11 +3,12 @@
 # >> EM QUE POSIÇÃO ELA APARECE A PRIMEIRA VEZ (FIND)
 # >> EM QUE POSIÇÃO ELA APARECE A ÚLTIMA VEZ
 
-frase = str(input('Digite uma frase: '))
+frase = str(input('Digite uma frase: ')).upper().strip()
 print('Analisando...')
 print('A letra \'A\' aparece {} vezes nessa frase.'.format(frase.count('A')))
-print('Ela aparece pela primeira vez na posição {}.'.format(frase.find('A')))
-print('E aparece pela última vez na posição {}.'.format(frase.find('A', frase.count('A') + frase.find('A'))))
+print('Ela aparece pela primeira vez na posição {}.'.format(frase.find('A') + 1))
+print('E aparece pela última vez na posição {}.'.format(frase.rfind('A') + 1))
+
 
 
 
